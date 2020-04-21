b94eb25fd9bfcbd311391b4518dd0142ed452504
Eric Iacutone <eric.iacutone@gmail.com>
2020-04-21 10:52:48 -0400

    FIXUP: Install and setup LiveView

--- lib/tech_interview_web/endpoint.ex
+++ lib/tech_interview_web/endpoint.ex
@@ -3,16 +3,13 @@ defmodule TechInterviewWeb.Endpoint do
 
   @session_options [
     store: :cookie,
-    key: "_my_app_key",
-    signing_salt: "somesigningsalt"
+    key: "_tech_interview_key",
+    signing_salt: "JI1ZoXiC"
   ]
 
   socket "/socket", TechInterviewWeb.UserSocket,
     websocket: true,
     longpoll: false
-
-  socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
@@ -47,5 +44,6 @@ defmodule TechInterviewWeb.Endpoint do
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
   plug Plug.Session, @session_options
+
   plug TechInterviewWeb.Router
 end
